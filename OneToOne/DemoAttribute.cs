6a769c04c1d65ccf38ef0c5eea60ff95fca31cb1
mas <305596979@qq.com>
2017-08-25 01:07:03 +0800

    睡觉前更新

---
+++ OneToOne/DemoAttribute.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneToOne
+{
+    /// <summary>
+    /// 映射到数据库中的表名
+    /// </summary>
+    [Table("Demo")]
+    public class DemoAttribute
+    {
+        /*
+                    数据注解
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+                Table：指定该类要映射到数据库中的表名
+                Column：指定类的属性要映射到数据表中的列名
+                Key：指定该属性是否以主键对待
+                TimeStamp：将该属性标记为数据库中的时间戳列
+                ForeignKey：指定一个导航属性的外键属性
+                NotMapped：指定该属性不应该映射到数据库中的任何列
+                DatabaseGenerated：指定属性应该映射到数据表中计算的列。也可以用于映射到自动增长的数据库表。
+         * 
+         * 
+         * 
+         */
+
+
+
+        /*
+        
+         组合主键
+         [Key]
+         [Column(Order=1)]
+         public int StudentKey1 { get; set; }
+
+         [Key]
+         [Column(Order=2)]
+         public int StudentKey2 { get; set; }
+         * 
+         */
+
+        [Key]//主键 默认设置为自动增长
+
+        [Column("ID")]//映射到数据表中的列名
+        public string ID { get; set; }
+
+
+        /// <summary>
+        /// GUID类型，则要手动的设置自增长
+        /// </summary>
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        //[Key]
+        public Guid UserGuid { get; set; }
+
+        [MaxLength(8, ErrorMessage = "名称长度不能大于8")]//最大长度
+        [MinLength(2, ErrorMessage = "名称长度不能小于2")]//最小长度
+        [StringLength(8, MinimumLength = 2, ErrorMessage = "名称长度不能大于8小于2")]//允许的字符的最小长度和最大长度
+        [Required]//值是必需的，不能为空
+
+        //当EF执行更新操作的时候，Code-First将列的值放在where条件语句中，
+        //你可以使用这个CurrencyCheck特性，使用已经存在的列做并发检查
+        [ConcurrencyCheck]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 指定一个导航属性的外键属性
+        /// </summary>
+        [ForeignKey("Person")]
+        public int PID { get; set; }
+
+        public virtual Person Person { get; set; }
+
+        [NotMapped]//不映射到数据库中的任何列
+        public int Age { get; set; }
+
+        /// <summary>
+        /// 给列设定的是tiemStamp类型。
+        /// 在并发的检查中，Code-First会自动使用这个TimeStamp类型的字段。
+        /// </summary>
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}
